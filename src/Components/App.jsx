e0b9de753306376112fb0a482d6a014c779439b4
guygreenleaf <yaweh23@gmail.com>
2021-10-11 13:10:40 -0700

    init commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,17 @@
+
+import '../Styles/App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Home from './Home';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
