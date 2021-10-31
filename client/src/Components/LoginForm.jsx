eb0158abed174a22e6477783adf5285ad9c9329e
guygreenleaf <yaweh23@gmail.com>
2021-10-31 12:03:42 -0700

    minor text styling fix

--- client/src/Components/LoginForm.jsx
+++ client/src/Components/LoginForm.jsx
@@ -33,10 +33,14 @@ function LoginForm(props) {
 
     //Login functions
     let handleEmailChange = (e) => {
+        setSuccess('');
+        setError('');
         setUserEmail(e.target.value);
     }
 
     let handlePasswordChange = (e) => {
+        setSuccess('');
+        setError('');
         setUserPassword(e.target.value);
     }
 
@@ -238,8 +242,8 @@ function LoginForm(props) {
                 </Container> 
 
                 <div style={{display: 'flex', flexDirection: 'row'}}>
-                    <p style={{color:'black', marginLeft: "25px"}}>Already have an account?</p>
-                    <p style={{color:'dodgerblue', cursor: 'pointer', marginLeft:"5px", textDecoration:'underline'}} onClick={swapForm}>Login</p> 
+                    <p style={{color:'black', marginLeft: "25px", fontSize: "14px"}}>Already have an account?</p>
+                    <p style={{color:'dodgerblue', cursor: 'pointer', marginLeft:"5px", textDecoration:'underline', fontSize: "14px"}} onClick={swapForm}>Login</p> 
                 </div>
             </div>
 
